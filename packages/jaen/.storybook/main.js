0205cb0125cb5c9414319413dd6191c2c483ce5f
Nico Schett <nicoschett@icloud.com>
2022-01-13 18:55:49 +0100

    add updatemodal for imagefield

--- packages/jaen/.storybook/main.js
+++ packages/jaen/.storybook/main.js
@@ -7,5 +7,15 @@ module.exports = {
     'storybook-addon-gatsby'
   ],
   framework: '@storybook/react',
-  core: {builder: 'webpack5'}
+  core: {builder: 'webpack5'},
+  webpackFinal: async config => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    config.module.rules[0].exclude = [/core-js/]
+
+    // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+
+    config.resolve.mainFields = ['browser', 'module', 'main']
+    return config
+  }
 }
