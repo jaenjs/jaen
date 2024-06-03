107cb88e59b3be39afebf5e6e6a1ae376256278c
Nico Schett <nicoschett@icloud.com>
2024-06-03 18:07:56 +0200

    feat: update usePage hook to support media injection
    This also fixes a issue where child pages do not include dynamic data.

--- packages/jaen/src/hooks/use-page.ts
+++ packages/jaen/src/hooks/use-page.ts
@@ -3,15 +3,16 @@ import {useEffect, useMemo, useState} from 'react'
 
 import {usePageContext} from '../contexts/page'
 import {RootState, store} from '../redux'
-import {JaenPage} from '../types'
+import {IJaenBlock, JaenPage, MediaNode} from '../types'
 import {useDynamicPaths} from './use-dynamic-paths'
 
 export interface UsePageProps {
   id?: string
   path?: string
+  injectMedia?: boolean
 }
 
-export const usePage = (props: UsePageProps): JaenPage => {
+export const usePage = (props: UsePageProps) => {
   const {jaenPage, jaenPages} = usePageContext()
 
   const paths = useDynamicPaths({
@@ -53,7 +54,66 @@ export const usePage = (props: UsePageProps): JaenPage => {
       const state = store.getState() as RootState
 
       const dynamicPage = state.page.pages.nodes[id]
-      setDynamicPage(dynamicPage)
+
+      const populateDynamicPage = (page: {id: string} & Partial<JaenPage>) => {
+        page.childPages = page.childPages?.map(childPage => {
+          const childPageData = state.page.pages.nodes[childPage.id]
+
+          let populatedPage: {id: string} & Partial<JaenPage> = {
+            ...childPage,
+            ...childPageData
+          }
+
+          // Recursively populate childPages
+          populatedPage = populateDynamicPage(populatedPage)
+
+          return populatedPage
+        })
+
+        const mediaPage =
+          store.getState().page.pages.nodes['JaenPage /cms/media/']
+
+        page.mediaNodes = (
+          Object.values(
+            mediaPage.jaenFields['IMA:MEDIA_NODES']?.media_nodes?.value || {}
+          ).filter((mediaNode: MediaNode) => {
+            return page.id === mediaNode.jaenPageId
+          }) || []
+        ).map((mediaNode: MediaNode) => {
+          return {
+            id: mediaNode.id,
+            node: {
+              childImageSharp: {
+                gatsbyImageData: {
+                  placeholder: {
+                    fallback: mediaNode.preview?.url
+                  },
+                  layout: 'constrained',
+                  images: {
+                    fallback: {
+                      src: mediaNode.url
+                    }
+                  },
+                  width: mediaNode.width,
+                  height: mediaNode.height
+                }
+              }
+            },
+            description: mediaNode.description || ''
+          }
+        })
+
+        return page
+      }
+
+      if (dynamicPage) {
+        setDynamicPage(
+          populateDynamicPage({
+            ...dynamicPage,
+            id
+          })
+        )
+      }
     })
 
     return () => {
@@ -65,7 +125,53 @@ export const usePage = (props: UsePageProps): JaenPage => {
     const staticPage =
       jaenPage.id === id ? jaenPage : jaenPages?.find(page => page.id === id)
 
-    return deepmerge(staticPage || {}, dynamicPage || {})
+    const mergedPage = deepmerge(staticPage || {}, dynamicPage || {})
+
+    if (props.injectMedia) {
+      // Deep replace all values of jaenFields[IMA:ImageField][name].value with the media node
+
+      let mediaNodes = mergedPage.mediaNodes
+
+      const replaceJaenFieldsImageValueIfImageField = (
+        value: JaenPage | IJaenBlock
+      ) => {
+        mediaNodes = value.mediaNodes || mediaNodes
+
+        for (const key in value.jaenFields) {
+          const field = value.jaenFields[key]!
+
+          if (key === 'IMA:ImageField') {
+            for (const value of Object.values(field)) {
+              const mediaNode = mediaNodes.find(
+                mediaNode => mediaNode.id === value.value
+              )
+
+              if (mediaNode) {
+                value.value = mediaNode
+              }
+            }
+          }
+        }
+      }
+
+      replaceJaenFieldsImageValueIfImageField(mergedPage)
+
+      // Process sections
+
+      for (const section of mergedPage.sections) {
+        for (const item of section.items) {
+          replaceJaenFieldsImageValueIfImageField(item)
+        }
+      }
+
+      // Process child pages
+
+      for (const childPage of mergedPage.childPages) {
+        replaceJaenFieldsImageValueIfImageField(childPage as JaenPage)
+      }
+    }
+
+    return mergedPage
   }, [id, dynamicPage])
 
   return page
