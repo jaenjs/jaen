4581e2d5d9dde8bb3f7e5b5bb1398446cc6570ea
Nico Schett <nicoschett@icloud.com>
2024-06-25 15:36:40 +0200

    save

--- packages/gatsby-jaen-lens/src/components/PasswordUpdateForm/PasswordUpdateForm.tsx
+++ packages/gatsby-jaen-lens/src/components/PasswordUpdateForm/PasswordUpdateForm.tsx
@@ -1,174 +1,163 @@
-import React, {useState} from 'react'
 import {
-  Box,
   Button,
-  FormControl,
-  FormErrorMessage,
-  FormHelperText, // Added FormHelperText
+  ButtonGroup,
+  FormControl, // Added FormHelperText
   FormLabel,
+  HStack,
   Input,
-  VStack,
-  Progress,
-  Text,
-  Stack,
-  Checkbox
+  List,
+  ListIcon,
+  ListItem,
+  Stack
 } from '@chakra-ui/react'
-import {useForm, Controller} from 'react-hook-form'
-import zxcvbn from 'zxcvbn'
-
-type PasswordResetFormValues = {
-  newPassword: string
-  confirmPassword: string
-  changeResourcePassword: boolean
-}
+import {FaCheck} from '@react-icons/all-files/fa/FaCheck'
+import {FaX} from '@react-icons/all-files/fa6/FaX'
+import React, {useState} from 'react'
 
 export interface PasswordUpdateFormProps {
-  onSubmit: (data: PasswordResetFormValues) => Promise<boolean>
-  resource: {name: string}
+  passwordPolicy: {
+    minLength: number
+    hasSymbol: boolean
+    hasNumber: boolean
+    hasUppercase: boolean
+    hasLowercase: boolean
+  }
+  onPasswordUpdate: (currentPassword: string, password: string) => Promise<void>
 }
 
 export const PasswordUpdateForm: React.FC<PasswordUpdateFormProps> = props => {
-  const [passwordStrength, setPasswordStrength] = useState<number | null>(null)
-  const [passwordSuggestions, setPasswordSuggestions] = useState<string[]>([])
+  const [currentPassword, setCurrentPassword] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>('')
 
-  const {
-    register,
-    handleSubmit,
-    getValues,
-    control,
-    formState: {errors, isSubmitting}
-  } = useForm<PasswordResetFormValues>({
-    defaultValues: {
-      changeResourcePassword: true
-    }
-  })
+  const [isPasswordChanging, setIsPasswordChanging] = useState(false)
 
-  const onSubmit = async (data: PasswordResetFormValues) => {
-    await props.onSubmit(data)
-  }
-
-  const checkPasswordStrength = (password: string) => {
-    const result = zxcvbn(password)
-    setPasswordStrength(result.score)
-    setPasswordSuggestions(result.feedback.suggestions)
-  }
-
-  const getPasswordStrengthColor = () => {
-    if (passwordStrength === null) {
-      return 'gray'
-    }
-    // Define color codes for different password strengths
-    const colors = ['red', 'orange', 'yellow', 'green', 'teal']
-    // Map the password strength score (0-4) to the colors
-    return colors[passwordStrength]
+  const handlePasswordChange = async () => {
+    setIsPasswordChanging(true)
+    await props.onPasswordUpdate(currentPassword, password)
+    setIsPasswordChanging(false)
   }
 
   return (
-    <Box
-      p={4}
-      borderWidth="1px"
-      borderRadius="md"
-      bg="bg.surface"
-      borderColor="border.emphasized">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <VStack spacing={8} align="stretch">
-          <Stack spacing="4">
-            <FormControl isInvalid={!!errors.newPassword}>
-              <FormLabel htmlFor="newPassword">New password</FormLabel>
-              <Controller
-                name="newPassword"
-                control={control}
-                rules={{
-                  required: 'New Password is required',
-                  validate: value => {
-                    // Custom validation for password strength
-                    const strengthResult = zxcvbn(value)
-                    if (strengthResult.score < 2) {
-                      return 'Password strength is insufficient'
-                    }
-                    return true
-                  }
-                }}
-                render={({field}) => (
-                  <Stack>
-                    <Input
-                      {...field}
-                      type="password"
-                      onChange={e => {
-                        field.onChange(e)
-                        checkPasswordStrength(e.target.value)
-                      }}
-                    />
-                    <Progress
-                      value={
-                        passwordStrength === null
-                          ? 0
-                          : (passwordStrength + 1) * 25
-                      }
-                      colorScheme={getPasswordStrengthColor()}
-                      size="sm"
-                      isAnimated
-                    />
-                  </Stack>
-                )}
-              />
-              {/* <FormHelperText>
-              Password Strength:{' '}
-              {
-                ['Very Weak', 'Weak', 'Fair', 'Strong', 'Very Strong'][
-                  passwordStrength || 0
-                ]
-              }
-            </FormHelperText> */}
-              {passwordSuggestions.length > 0 && (
-                <FormHelperText color="red">
-                  Suggestions: {passwordSuggestions.join(' ')}
-                </FormHelperText>
-              )}
-              <FormErrorMessage>
-                {errors.newPassword && errors.newPassword.message}
-              </FormErrorMessage>
-            </FormControl>
+    <Stack spacing="6">
+      <FormLabel>
+        Enter the new password according to the policy below.
+      </FormLabel>
+      <FormControl>
+        <FormLabel>Current Password</FormLabel>
+        <Input
+          maxW="md"
+          type="password"
+          placeholder="New password"
+          onChange={e => setCurrentPassword(e.target.value)}
+        />
+      </FormControl>
 
-            <FormControl isInvalid={!!errors.confirmPassword}>
-              <FormLabel htmlFor="confirmPassword">Confirm password</FormLabel>
-              <Controller
-                name="confirmPassword"
-                control={control}
-                rules={{
-                  required: 'Confirm Password is required',
-                  validate: value =>
-                    value === getValues('newPassword') ||
-                    'Passwords do not match'
-                }}
-                render={({field}) => <Input {...field} type="password" />}
-              />
-              <FormErrorMessage>
-                {errors.confirmPassword && errors.confirmPassword.message}
-              </FormErrorMessage>
-            </FormControl>
-          </Stack>
+      <List spacing={3}>
+        {props.passwordPolicy.minLength && (
+          <ListItem>
+            {password.length >= props.passwordPolicy.minLength ? (
+              <ListIcon as={FaCheck} color="green.500" />
+            ) : (
+              <ListIcon as={FaX} color="red.500" />
+            )}
+            Has to be at least {props.passwordPolicy.minLength} characters long.
+            ({password.length} / {props.passwordPolicy.minLength})
+          </ListItem>
+        )}
+        {props.passwordPolicy.hasSymbol && (
+          <ListItem>
+            {/[\p{P}\p{S}]/u.test(password) ? (
+              <ListIcon as={FaCheck} color="green.500" />
+            ) : (
+              <ListIcon as={FaX} color="red.500" />
+            )}
+            Must include a symbol or punctuation mark.
+          </ListItem>
+        )}
 
-          <FormControl isInvalid={!!errors.changeResourcePassword}>
-            <Stack direction="row" align="center">
-              <Checkbox
-                {...register('changeResourcePassword', {
-                  required: false
-                })}>
-                Change {props.resource.name} password
-              </Checkbox>
-            </Stack>
-            <FormHelperText>
-              When checked, the password reset will change all internal
-              passwords and the main password.
-            </FormHelperText>
-          </FormControl>
+        {props.passwordPolicy.hasNumber && (
+          <ListItem>
+            {/\d/.test(password) ? (
+              <ListIcon as={FaCheck} color="green.500" />
+            ) : (
+              <ListIcon as={FaX} color="red.500" />
+            )}
+            Must include a number.
+          </ListItem>
+        )}
 
-          <Button type="submit" isLoading={isSubmitting}>
-            Update Password
-          </Button>
-        </VStack>
-      </form>
-    </Box>
+        {props.passwordPolicy.hasUppercase && (
+          <ListItem>
+            {/[A-Z]/.test(password) ? (
+              <ListIcon as={FaCheck} color="green.500" />
+            ) : (
+              <ListIcon as={FaX} color="red.500" />
+            )}
+            Must include an uppercase letter.
+          </ListItem>
+        )}
+
+        {props.passwordPolicy.hasLowercase && (
+          <ListItem>
+            {/[a-z]/.test(password) ? (
+              <ListIcon as={FaCheck} color="green.500" />
+            ) : (
+              <ListIcon as={FaX} color="red.500" />
+            )}
+            Must include a lowercase letter.
+          </ListItem>
+        )}
+
+        <ListItem>
+          {password && password === passwordConfirmation ? (
+            <ListIcon as={FaCheck} color="green.500" />
+          ) : (
+            <ListIcon as={FaX} color="red.500" />
+          )}
+          Passwords match.
+        </ListItem>
+      </List>
+
+      <HStack>
+        <FormControl>
+          <FormLabel>New Password</FormLabel>
+          <Input
+            type="password"
+            placeholder="New password"
+            autoComplete="new-password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Confirm Password</FormLabel>
+          <Input
+            type="password"
+            placeholder="Confirm password"
+            autoComplete="new-password"
+            onChange={e => setPasswordConfirmation(e.target.value)}
+          />
+        </FormControl>
+      </HStack>
+
+      <ButtonGroup>
+        <Button
+          isLoading={isPasswordChanging}
+          type="submit"
+          onClick={handlePasswordChange}>
+          Reset Current Password
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            // reset
+            setCurrentPassword('')
+            setPassword('')
+            setPasswordConfirmation('')
+          }}>
+          Cancel
+        </Button>
+      </ButtonGroup>
+    </Stack>
   )
 }
